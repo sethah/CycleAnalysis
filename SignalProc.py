ee65d18731a87ea763c85c67be1014b79e225597
sethah <seth.hendrickson16@gmail.com>
2015-02-14 21:00:04 -0800

    Features dataframe method

--- SignalProc.py
+++ SignalProc.py
@@ -1,6 +1,7 @@
 import numpy as np
 from math import factorial
 
+
 def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     try:
         window_size = np.abs(np.int(window_size))
@@ -27,16 +28,86 @@ def savitzky_golay(y, window_size, order, deriv=0, rate=1):
     
     output = np.convolve(m[::-1], y_new, mode='valid')
 
-    if output.shape[0] == y.shape[0]:
-        print y.shape[0], output.shape[0]
+    # if output.shape[0] == y.shape[0]:
+    #     print y.shape[0], output.shape[0]
 
     return output
 
-def smooth(vec, copy=True):
+def scipy_smooth(x,window_len=11,window='hanning'):
+    """smooth the data using a window with requested size.
+    
+    This method is based on the convolution of a scaled window with the signal.
+    The signal is prepared by introducing reflected copies of the signal 
+    (with the window size) in both ends so that transient parts are minimized
+    in the begining and end part of the output signal.
+    
+    input:
+        x: the input signal 
+        window_len: the dimension of the smoothing window; should be an odd integer
+        window: the type of window from 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'
+            flat window will produce a moving average smoothing.
+
+    output:
+        the smoothed signal
+        
+    example:
+
+    t=linspace(-2,2,0.1)
+    x=sin(t)+randn(len(t))*0.1
+    y=smooth(x)
+    
+    see also: 
+    
+    numpy.hanning, numpy.hamming, numpy.bartlett, numpy.blackman, numpy.convolve
+    scipy.signal.lfilter
+ 
+    TODO: the window parameter could be the window itself if an array instead of a string
+    NOTE: length(output) != length(input), to correct this: return y[(window_len/2-1):-(window_len/2)] instead of just y.
+    """
+
+    if x.ndim != 1:
+        raise ValueError, "smooth only accepts 1 dimension arrays."
+
+    if x.size < window_len:
+        # print "Input vector needs to be bigger than window size."
+        return x
+
+
+    if window_len<3:
+        return x
+
+
+    if not window in ['flat', 'hanning', 'hamming', 'bartlett', 'blackman']:
+        raise ValueError, "Window is on of 'flat', 'hanning', 'hamming', 'bartlett', 'blackman'"
+
+
+    s=np.r_[x[window_len-1:0:-1],x,x[-1:-window_len:-1]]
+    #print(len(s))
+    if window == 'flat': #moving average
+        w=np.ones(window_len,'d').astype(float)
+    else:
+        w=eval('np.'+window+'(window_len)').astype(float)
+
+    y=np.convolve(w/w.sum(),s,mode='valid')
+    output = y[(window_len/2):-(window_len/2)]
+
+    if output.shape[0] != x.shape[0]:
+        print x.shape[0], output.shape[0]
+
+    return output
+
+
+def smooth(vec, filt_type, copy=True):
     if copy:
         vec = np.copy(vec)
 
-    return savitzky_golay(vec.ravel(), 51, 3)
+    if filt_type == 'savgol':
+        smoothed = savitzky_golay(vec.ravel(), 51, 3)
+    elif filt_type == 'scipy':
+        smoothed = scipy_smooth(vec.ravel())
+
+    return smoothed
+
 
 def weighted_average(vals, weights):
     w = np.diff(weights)
--- StravaUser.py
+++ StravaUser.py
@@ -2,7 +2,10 @@ import numpy as np
 import pandas as pd
 import pymongo
 from SignalProc import weighted_average, smooth, diff
+from sklearn.cross_validation import train_test_split, cross_val_score
+from sklearn.ensemble import RandomForestRegressor
 from datetime import datetime
+from StravaEffort import StravaActivity
 import matplotlib.pyplot as plt
 from PlotTools import PlotTool
 import seaborn as sns
@@ -35,286 +38,38 @@ class StravaUser(object):
             if a.is_ride() and len(a.distance.raw_data) > min_length:
                 self.activities.append(a)
 
-class StravaEffort(object):
+    def make_df(self, activities=None):
+        if activities is not None:
+            start = activities[0]
+            stop = activities[1]
+        else:
+            start = 0
+            stop = len(self.activities)
 
-    def __init__(self, effort_dict):
-        self.init(effort_dict)
-
-    def init(self, effort_dict):
-        self.id = effort_dict['id']
-        self.name = effort_dict['name']
-        self.athlete = effort_dict['athlete']#['id']
-        self.date = self.strava_date(effort_dict['start_date'])
-        stream_dict = effort_dict['streams']
-        self.velocity = self.init_stream(stream_dict, 'velocity_smooth')
-        self.distance = self.init_stream(stream_dict, 'distance')
-        self.time = self.init_stream(stream_dict, 'time')
-        self.grade = self.init_stream(stream_dict, 'grade_smooth')
-        self.altitude = self.init_stream(stream_dict, 'altitude')
-
-    def init_stream(self, stream_dict, stream_type):
-        return StravaStream(stream_type, stream_dict[stream_type])
-
-    def stream_types(self):
-        return {'velocity_smooth', 'distance', 'time', 'grade_smooth', 'altitude'}
-
-    def strava_date(self, date_string):
-        if type(date_string) != unicode:
-            return date_string
-        return datetime.strptime(date_string, '%Y-%m-%dT%H:%M:%SZ')
-
-class StravaActivity(StravaEffort):
-
-    def __init__(self, activity_dict):
-        self.init(activity_dict)
-        self.distance.convert_units()
-        self.velocity.convert_units()
-        self.altitude.convert_units()
-        self.city = activity_dict['location_city']
-        self.total_distance = activity_dict['distance']
-        self.hills = self.hill_analysis()
-        self.is_valid_ride = self.is_ride()
-
-    def init_stream(self, stream_dict, stream_type):
-        return StravaStream(stream_type, stream_dict[stream_type])
-
-    def is_ride(self):
-        min_vel = 4.  # mph
-        max_vel = 30.  # mph
-
-        avg_vel = np.mean(self.velocity.raw_data)
-        return avg_vel >= min_vel and avg_vel <= max_vel
-
-    def hill_analysis(self):
-        diff_alt = diff(self.altitude.raw_data[:, np.newaxis],
-                        self.distance.raw_data[:, np.newaxis])
-        diff_alt = smooth(diff_alt)
-
-        hills = []
-        hill_start = False
-        hill_start_idx = 0
-        hill_start_time = 0
-        stream_length = self.distance.raw_data.shape[0]
-        climb_total = np.zeros(stream_length)
-        for k in xrange(stream_length):
-            time_elapsed = self.time.raw_data[k] - hill_start_time
-            if k != 0:
-                climb = np.max([self.altitude.raw_data[k] - self.altitude.raw_data[k - 1], 0])
-                climb_total[k] = climb_total[k - 1] + climb
-
-            if diff_alt[k] > 0 and not hill_start:
-                hill_start = True
-                hill_start_idx = k
-                hill_start_time = self.time.raw_data[hill_start_idx]
-
-            elif hill_start and (diff_alt[k] < 0 or k == stream_length - 1):
-                hill_start = False
-                if len(hills) != 0:
-                    prev_hill = hills[-1]
-                else:
-                    prev_hill = None
-                if k - hill_start_idx >= 2:
-                    stream_dict = self.make_stream_dict()
-                    for key in stream_dict:
-                        stream_dict[key]['data'] = stream_dict[key]['data'][hill_start_idx:k]
-
-                    effort_dict = {'id': len(hills),
-                                   'name': self.name,
-                                   'athlete': self.athlete,
-                                   'start_date': self.date,
-                                   'streams': stream_dict}
-                    hill = StravaHill(effort_dict, self, previous_hill=prev_hill)
-                    hills.append(hill)
-
-        return self.clean_hills(hills)
-
-    def make_stream_dict(self):
-        return {stream_type: {'data': getattr(self, stream_type.replace('_smooth', '')).raw_data} \
-                for stream_type in self.stream_types()}
-
-    def clean_hills(self, hills):
-        min_score = 8000. / meters_per_mile
-        min_separation = 0.5  # miles
-        clean_hills = []
-        for k, hill in enumerate(hills):
-            if len(clean_hills) != 0:
-                if hill.distance.raw_data[0] - clean_hills[-1].distance.raw_data[-1] < min_separation:
-                    hill = self.merge_hills(clean_hills[-1], hill)
-                    clean_hills.pop()
-            if hill.score > min_score:
-                if hill.previous_hill is not None:
-
-                    if hill.previous_hill.score < min_score:
-                        hill.previous_hill = None
-                clean_hills.append(hill)
-
-        return clean_hills
-
-    def merge_hills(self, hill1, hill2):
-        stream_dict = {}
-        for stream_type in self.stream_types():
-            hill1_data = getattr(hill1, stream_type.replace('_smooth','')).raw_data
-            hill2_data = getattr(hill2, stream_type.replace('_smooth','')).raw_data
-            stream_dict[stream_type] = {}
-            stream_dict[stream_type]['data'] = np.append(hill1_data, hill2_data)
-        effort_dict = {'id': hill1.id,
-                       'name': hill1.name,
-                       'athlete': hill1.athlete,
-                       'start_date': hill1.date,
-                       'streams': stream_dict}
-        hill = StravaHill(effort_dict, self, previous_hill=hill1.previous_hill)
-
-        return hill
-
-    def plot_hills(self):
-        fig, ax = plt.subplots(1, 1, figsize=(12, 8))
-        p = PlotTool()
-        ax.plot(self.distance.raw_data, self.altitude.raw_data,
-                c='r', label='altitude')
-        xmin, xmax = np.min(self.distance.raw_data), np.max(self.distance.raw_data)
-        ymin, ymax = np.min(self.altitude.raw_data), np.max(self.altitude.raw_data)
-        ax.set_xlim([xmin, xmax])
-        ax.set_ylim([ymin, ymax])
-        ax.set_title(self.date.date())
-        axis_label_font = 20
-        ax.set_xlabel('Distance (miles)', fontsize=axis_label_font)
-        ax.set_ylabel('Altitude (ft)', fontsize=axis_label_font)
-        ax.set_title('%s, %s, %s' % \
-                        (self.name, self.athlete['id'], self.date.date()),
-                        fontsize=24)
-
-        ax.tick_params(labelsize=16)
-
-        cmap = plt.get_cmap("YlOrRd")
-
-        for hill in self.hills:
-            label = 'Score: %0.0f' % (hill.score)
-            X = hill.distance.raw_data
-            Y = hill.altitude.raw_data
-            p.plot_fill(X, Y, hill.velocity.filtered, cmap, ax)
-
-        plt.show()
-
-    def plot_each_hill(self):
-        p = PlotTool()
-        r, c = p.subplot_dims(len(self.hills))
-        fig, axs = plt.subplots(r, c, figsize=(15, 12))
-        
-        if r == 1 and c == 1:
-            axs = np.array([axs])
-
-        cmap = plt.get_cmap("autumn")
-        for k, ax in enumerate(axs.reshape(-1)):
-            hill = self.hills[k]
-            label = 'Score: %0.0f' % (hill.score)
-            X = hill.distance.raw_data
-            Y = hill.altitude.raw_data
-            p.plot_fill(X, Y, hill.velocity.filtered, cmap, ax)
-            ax.set_xlabel('Distance (miles)')
-            ax.set_ylabel('Altitude (feet)')
-        fig.suptitle('Hills for %s on %s' % (self.name, self.date.date()),
-                     fontsize=20)
-        plt.show()
-
-    def hills_df(self):
         df = None
-        for hill in self.hills:
+        for a in self.activities[start:stop]:
             if df is None:
-                df = hill.make_df()
+                df = a.hills_df()
             else:
-                df = df.append(hill.make_df(), ignore_index=True)
+                df = df.append(a.hills_df(), ignore_index=True)
 
         return df
 
-    def __repr__(self):
-        return '<%s, %s, %s, %s>' % \
-            (self.name, self.date, self.city, len(self.distance.raw_data))
-
-
-class StravaStream(object):
-
-    def __init__(self, stream_type, stream_dict, change_units=True):
-        self.raw_data = np.array(stream_dict['data'])
-        self.stream_type = stream_type
-        self.filter()
-    
-    def filter(self):
-        self.filtered = smooth(self.raw_data)
-
-    def convert_units(self):
-        if self.stream_type == 'distance':
-            self.raw_data /= meters_per_mile
-        elif self.stream_type == 'velocity':
-            self.raw_data *= 3600
-            self.raw_data /= meters_per_mile
-        elif self.stream_type == 'altitude':
-            self.raw_data *= feet_per_meter
-
-
-class StravaHill(StravaEffort):
-
-    def __init__(self, effort_dict, activity, previous_hill=None):
-        self.init(effort_dict)
-        self.activity = activity
-        self.previous_hill = previous_hill
-        self.score = self.hill_score()
-
-    def hill_score(self):
-        grade_vec = self.grade.raw_data
-        dist_vec = self.distance.raw_data
-
-        mean_grade = weighted_average(grade_vec, dist_vec)
-        distance = self.distance.raw_data[-1] - self.distance.raw_data[0]
-
-        return distance*mean_grade
-
-    def hill_features(self):
-        self.previous_climb = self.past_climb()
-        # self.time_elapsed = self.time.raw_data[0]
-        # self.distance_elapsed = self.distance.raw_data[0]
-        if self.previous_hill is not None:
-            self.time_since_last_climb = self.time.raw_data[0] - self.previous_hill.time.raw_data[-1]
-            self.score_last_climb = self.previous_hill.score
-        else:
-            self.time_since_last_climb = 100000
-            self.score_last_climb = 0
-
-    def start_index(self):
-        hill_start_time = self.time.raw_data[0]
-        start = (np.abs(self.activity.time.raw_data-hill_start_time)).argmin()
-        return start
-
-    def past_climb(self):
-        start = self.start_index()
-        # print start
-        diff = np.diff(self.activity.altitude.raw_data[0:start])
-        return np.sum(np.where(diff < 0, 0, diff))
-
-    def make_df(self):
-        self.hill_features()
-        n = self.grade.filtered.shape[0]
-        d = {'activity_id': [self.activity.id]*n,
-             'hill_id': [self.id]*n,
-             'previous_climb': [self.previous_climb]*n,
-             'time_since_last_climb': [self.time_since_last_climb]*n,
-             'score_last_climb': [self.score_last_climb]*n,
-             'grade': self.grade.filtered,
-             'time': self.time.raw_data,
-             'distance': self.distance.raw_data,
-             'altitude': self.altitude.raw_data,
-             'velocity': self.velocity.filtered
-             }
-        # for key in d:
-        #     print key, len(d[key])
-
-        return pd.DataFrame(d)
-
-    def __repr__(self):
-        return '<%s, %s>' % \
-            (self.score, len(self.distance.raw_data))
 
 if __name__ == '__main__':
     u = StravaUser('Seth')
-    u.activities[3].plot_hills()
-    # for hill in u.activities[2].hills:
-    #     print hill.score, 8000 / meters_per_mile
+    df = u.make_df()
+    df = df[df['velocity'] > 3]
+    y = df.pop('velocity')
+    # df.pop('altitude')
+    X = df.values
+    X_train, X_test, y_train, y_test = train_test_split(X, y)
+
+    rf = RandomForestRegressor()
+    rf.fit(X_train, y_train)
+    # rf.score(X_test, y_test)
+    cvscore = cross_val_score(rf, X_train, y_train, cv=10, scoring='r2')
+    print np.mean(cvscore)
+    importances = sorted(zip(rf.feature_importances_, df.columns), key=lambda x: x[0])
+    for feature in importances:
+        print feature
